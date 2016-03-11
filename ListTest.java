d358b9f6181f4f2bda00892dcd8cf9914acb911b
bkuschner <bkusc001@ucr.edu>
2016-03-11 06:37:29 -0800

    Added files via upload

---
+++ ListTest.java
@@ -0,0 +1,14 @@
+
+public class ListTest {
+	public static void main (String[] args) {
+		LinkedList myList = new LinkedList();
+		
+		myList.insert("Blastoff!");
+		myList.insert("one");
+		myList.insert("two");
+		myList.insert("three");
+		myList.insert("four");
+		myList.insert("five");
+		myList.printList();
+	}
+}
